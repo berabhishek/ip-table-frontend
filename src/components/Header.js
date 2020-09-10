1d545ec0e4be6b9b99cbcb0362d3156115fc243c
Abhishek Bera <bera.abhishek21@gmail.com>
2020-09-10 22:45:05 +0530

    Removed unused links

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,12 +8,6 @@ class Header extends React.Component {
                     <div class="mdl-layout__header-row">
                     <span class="mdl-layout-title">{this.props.title}</span>
                     <div class="mdl-layout-spacer"></div>
-                    <nav class="mdl-navigation mdl-layout--large-screen-only">
-                        <a class="mdl-navigation__link" href="">Link</a>
-                        <a class="mdl-navigation__link" href="">Link</a>
-                        <a class="mdl-navigation__link" href="">Link</a>
-                        <a class="mdl-navigation__link" href="">Link</a>
-                    </nav>
                     </div>
                 </header>
             </div>
